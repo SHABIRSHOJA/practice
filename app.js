8822e7f2980750828a663f26e3f5abf55c162e30
Shabir Ahmad Shoja <119832741+SHABIRSHOJA@users.noreply.github.com>
2024-03-01 07:18:00 +0000

    new-

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const { Socket } = require('dgram');
+const http = require('http');
+const { json } = require('stream/consumers');
+
+const server = http.createServer((req, res) =>{
+    if(req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
+
+
+    if(req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000....');
