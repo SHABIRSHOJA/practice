2efb5e2bbbf13c4f3c1b268acfb9f55481d2e239
Shabir Ahmad Shoja <119832741+SHABIRSHOJA@users.noreply.github.com>
2024-03-02 05:14:49 +0000

    new

--- Express/index.js
+++ Express/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app =  express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/get", (req, res) => {
+
+    res.send("Hello Shabir");
 });
 
+
+app.get("/trianing", (req, res) => {
+
+    var trainingObj = {
+        trainingId: 10,
+        trainingName: 'Express Course',
+        active: 'True'
+    }
+    res.send(trainingObj);
+});
 app.listen(4000);
